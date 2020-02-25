f4a47ded738186a3d127219d109661a167b68c9c
Alexei Aj <alexei.junior@hotmail.com>
2020-02-25 15:23:47 -0300

    tcp

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+
+public class Client : MonoBehaviour {
+    public static Client instance = null;
+    private string ip = "127.0.0.1";
+    private int port = 8080;
+    private int id = 0;
+    private Tcp tcp;
+
+    private void Awake() {
+        if (instance != null && instance != this){
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
+    }
+
+    public void connectToServer() {
+        tcp = new Tcp(); 
+        tcp.connect(ip, port);
+    }
+
+    public void sendMsg(string msg) {
+        Packet packet = new Packet();
+        packet.Write(msg);
+        packet.Write(UIManager.instance.username.text);
+        packet.Write(id);
+
+        sendTcpData(packet);
+    }
+
+    private void sendTcpData(Packet packet) {
+        packet.WriteLength();
+        tcp.sendData(packet);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    public static UIManager instance = null;
+    public GameObject startMenu;
+    public InputField username;
+
+    private void Awake() {
+        if (instance != null && instance != this){
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
+    }
+
+    public void connectToServer() {
+        Debug.Log("Conectando!");
+        startMenu.SetActive(false);
+        username.interactable = false;
+        Client.instance.connectToServer();
+    }
+}
